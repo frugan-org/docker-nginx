9fe42dadaf658a9e2ea3da9f32af3ff7e708858a
Frugan <dev@frugan.it>
2024-03-03 16:05:01 +0100

    feat!: add support up to v1.25
    replace ENV with APP_ENV
    shellcheck and lint scripts
    update docs

--- deploy.sh
+++ deploy.sh
@@ -3,32 +3,37 @@
 #https://blog.dockbit.com/templating-your-dockerfile-like-a-boss-2a84a67d28e9
 
 deploy() {
-  str="
+	str="
   s!%%TAG%%!$TAG!g;
 "
 
-  sed -r "$str" $1
+	sed -r "$str" "$1"
 }
 
 TAGS=(
-  1.21
-  1.22
-  1.23
+	1.21
+	1.22
+	1.23
+	1.24
+	1.25
 )
 
 ENTRYPOINT=entrypoint.sh
 
+IFS='
+'
+# shellcheck disable=SC2048
 for TAG in ${TAGS[*]}; do
 
-  if [ -d "$TAG" ]; then
-    rm -Rf $TAG
-  fi
+	if [ -d "$TAG" ]; then
+		rm -Rf "$TAG"
+	fi
 
-  mkdir $TAG
-  deploy Dockerfile.template > $TAG/Dockerfile
+	mkdir "$TAG"
+	deploy Dockerfile.template >"$TAG"/Dockerfile
 
-  if [ -f "$ENTRYPOINT" ]; then
-    cp $ENTRYPOINT $TAG
-  fi
+	if [ -f "$ENTRYPOINT" ]; then
+		cp $ENTRYPOINT "$TAG"
+	fi
 
 done
